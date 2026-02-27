7218d80e1150f0c9660e2a67daba41457b66b066
Stephen Matula <smatula@redhat.com>
2026-02-27 12:28:16 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World - Updated! 1770825721491 !"
+
+if __name__ == '__main__':
+    port = os.environ.get('FLASK_PORT') or 8080
+    port = int(port)
+
+    app.run(port=port,host='0.0.0.0')
